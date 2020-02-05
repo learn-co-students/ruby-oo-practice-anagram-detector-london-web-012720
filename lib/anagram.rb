deffdf2808073f3a3584192ac0a202146ccbeaa4
Amy Franz <amy.franz@gmail.com>
2020-02-05 11:32:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :compare_word
+
+    def initialize (compare_word)
+        @compare_word = compare_word
+    end
+
+    def sort_word(word)
+        word.sort
+    end
+
+    def split_word(word)
+        word.split("")
+    end
+
+    def compare_word(word1, word2)
+        word1 == word2
+    end
+
+    def match(match)
+        comp_word = split_word(@compare_word)
+        comp_word = sort_word(comp_word)
+        matched_list = []
+
+        match.map do |w|
+            m = split_word(w)
+            m = sort_word(m)
+            matched_list << w if compare_word(m, comp_word)
+        end
+        return matched_list
+    end
+end
